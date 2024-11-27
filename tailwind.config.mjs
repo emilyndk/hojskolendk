78147ad4cee6ed488024bfecdb3bd5806bf02c34
Emily Nielsen <emily.nielsen09@gmail.com>
2024-11-27 11:13:58 +0100

    b

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,7 +15,7 @@ export default {
         yellow: {
           500: '#FFFDF4',
         },
-        neutrals: {
+        neutral: {
           950: '#1A1A1A',
         },
       },
@@ -30,7 +30,9 @@ export default {
       },
   
       fontFamily: {
-        sans: ['blissmood', 'sans-serif'],
+        blissmood: ['blissmood', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
       },
   
       fontSize: {
@@ -52,7 +54,45 @@ export default {
         none: '0px',
       },
   
-      extend: {},
+      extend: {
+        fontSize: {
+          // Display
+          'L-hero-desktop': ['45px', { lineHeight: '140%' }],
+          'S-hero-mobile': ['45px', { lineHeight: '140%' }],
+          'L-card-desktop': ['48px', { lineHeight: '67px' }],
+          'S-card-mobile': ['32px', { lineHeight: '30px' }],
+  
+          // Headings H1
+          'h1-desktop-L': ['46px', { lineHeight: '45px' }],
+          'h1-mobile-S': ['34px', { lineHeight: '35px' }],
+          'h1-alt-desktop-L': ['32px', { lineHeight: '37px' }],
+          'h1-alt-mobile-S': ['24px', { lineHeight: '34px' }],
+  
+          // Headings H2
+          'h2-desktop-L': ['46px', { lineHeight: '37px', fontWeight: '300' }],
+          'h2-mobile-L': ['24px', { lineHeight: '30px', fontWeight: '300' }],
+          'h2-desktop-S': ['26px', { lineHeight: '37px', fontWeight: '300' }],
+          'h2-mobile-S': ['22px', { lineHeight: '24px', fontWeight: '300' }],
+  
+          // Body Text
+          'body-desktop-regular': ['16px', { lineHeight: '22px', fontWeight: '300' }],
+          'body-mobile-regular': ['12px', { lineHeight: '18px', fontWeight: '300' }],
+
+          'body-desktop-bold': ['24px', { lineHeight: '31px', fontWeight: '700' }],
+          'body-mobile-bold': ['16px', { lineHeight: '25px', fontWeight: '700' }],
+          
+         
+          
+  
+          // Nav
+          'body-nav-desktop': ['24px', { fontWeight: '700' }],
+          'body-nav-mobile': ['24px', { fontWeight: '700' }],
+  
+          // Button
+          'button-desktop': ['20px', { lineHeight: '140%' }],
+          'button-mobile': ['10px', { lineHeight: '140%' }],
+        },
+      },
     },
   
     plugins: [
