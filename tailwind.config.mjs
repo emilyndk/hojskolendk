24b3598da21932ab20daa70fc86b666c131eafb6
Emily Nielsen <emily.nielsen09@gmail.com>
2024-11-27 10:23:19 +0100

    l

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,87 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-
-    /* Vil du overskrive eller extende tailwinds indstillinger?  */
-
-	theme: {
-
-        /* Tilføj din egen farvepalette nedenfor */
-        colors: {
-             transparent: 'transparent',
-             current: 'currentColor',
-             'orange': {
-             
-             500: '#FF5E04',
-            
-         },
-         'green': {
-              
-              500: '#2D3F27',
-              
-         },
-         'yellow': {
-              
-              500: '#FFFDF4',
-              
-         },
-         'neutrals': {
-              
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        orange: {
+          500: '#FF5E04',
+        },
+        green: {
+          500: '#2D3F27',
+        },
+        yellow: {
+          500: '#FFFDF4',
+        },
+        neutrals: {
           950: '#1A1A1A',
-          
-     },
         },
-
-        
-        /* Tilføj din egen spacing nedenfor */
-        spacing: {
-            'card-34': '34px',
-            'card-106': '106px',
-            'card-124': '124px',
-            'section-72': '72px',
-            'h1': '22px',
-            'h2': '28px',
-        },
-        
- 
-
-        /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
-        // fontFamily: {
-        //     sans: ['Prompt', 'sans-serif'],
-        //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
-        // },
-
-        /* Tilføj dit typografiske hierarki herunder */
-        // fontSize: {
-        //     base: ['1rem', { lineHeight: '1.5rem' }],
-        //     large: ['2.5rem', { lineHeight: '3.75rem' }],
-        //   },
-
-        /* Tilføj dit fontvægt-hierarki nedenfor */
-        //   fontWeight: {
-        //     regular: '400',
-        //   },
-
-        /* Borders - borderstørrelser herunder */
-        borderWidth: {
-            DEFAULT: '1px',
-       //     0: '0px',
-       //     2: '2px',
-        },
-       /* Border radius størrelser herunder */
-        borderRadius: {
-           DEFAULT: '8px',
-            none: '0px',
-        },
-
-
-		extend: {
-        },
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
+      },
+  
+      spacing: {
+        'card-34': '34px',
+        'card-106': '106px',
+        'card-124': '124px',
+        'section-72': '72px',
+        h1: '22px',
+        h2: '28px',
+      },
+  
+      fontFamily: {
+        sans: ['blissmood', 'sans-serif'],
+      },
+  
+      fontSize: {
+        base: ['1rem', { lineHeight: '1.5rem' }],
+        large: ['2.5rem', { lineHeight: '3.75rem' }],
+      },
+  
+      fontWeight: {
+        regular: '400',
+        bold: '700',
+      },
+  
+      borderWidth: {
+        DEFAULT: '1px',
+      },
+  
+      borderRadius: {
+        DEFAULT: '8px',
+        none: '0px',
+      },
+  
+      extend: {},
+    },
+  
+    plugins: [
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+      require('@tailwindcss/container-queries'),
     ],
-}
+  };
+  
